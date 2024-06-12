6a2924cf1e131feb4ae3ad3aa4cc36b231516dbe
HieuNghia2k1 <boybin1707@gmail.com>
2024-06-13 01:11:59 +0700

    2

---
+++ QuanLyNhanVien/js/index.js
@@ -0,0 +1,7 @@
+// Thêm sinh viên vào mảng
+let arrSinhVien = [];
+document.getElementById("formQLSV").onsubmit = function (event) {
+  event.preventDefault();
+  let arrField = document.querySelectorAll("#formQLSV input, #formQLSV select");
+  console.log(arrField);
+};
